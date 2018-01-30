a368f1647cfb505d72e6341deb95a3f5dd4c971a
EstiakOmar <e.omar@dal.ca>
2018-01-30 12:40:53 -0400

    tested from different angle

--- D_list_comb.py
+++ D_list_comb.py
@@ -3,7 +3,7 @@ with open('LIWC_words.txt', 'r') as document:
     for line in document:
         line = line.split()
         #print(line)
-        if not line:  # empty line?
+        if not line:  # empty line
             continue
         #print(line[0], line[1:])
         answer[line[0]] = line[1:]
