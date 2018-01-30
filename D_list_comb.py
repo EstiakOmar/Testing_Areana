6707ff9146fa5fc057dd8871627153ca3b5f161a
EstiakOmar <e.omar@dal.ca>
2018-01-30 12:25:07 -0400

    2nd commit

---
+++ D_list_comb.py
@@ -0,0 +1,11 @@
+with open('LIWC_words.txt', 'r') as document:
+    answer = {}
+    for line in document:
+        line = line.split()
+        #print(line)
+        if not line:  # empty line?
+            continue
+        #print(line[0], line[1:])
+        answer[line[0]] = line[1:]
+        #break
+print(answer)
