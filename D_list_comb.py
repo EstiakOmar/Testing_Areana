666f7683fb19b5e49c9c2bddf8ddd7dd2664347b
EstiakOmar <e.omar@dal.ca>
2018-01-30 12:33:19 -0400

    Final Commit

--- D_list_comb.py
+++ D_list_comb.py
@@ -2,7 +2,7 @@ with open('LIWC_words.txt', 'r') as document:
     answer = {}
     for line in document:
         line = line.split()
-        print(line)
+        #print(line)
         if not line:  # empty line?
             continue
         #print(line[0], line[1:])
